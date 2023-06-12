55e1513130ea94199f41d37380e4f49871671782
MatiasSilvaIslas <matias.silva@alumnos.frgp.utn.edu.ar>
2023-06-12 12:50:29 -0300

    Agrego clases en dominio

---
+++ TPCuatrimestral-Album/dominio/Figurita.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Figurita
+    {
+        public int IdFigurita { get; set; }
+        public TipoDeFigurita TipoDeFigurita { get; set; }
+        public Album Album { get; set; }
+        public bool Pegada { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ TPCuatrimestral-Album/dominio/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public decimal Monedas { get; set; }
+        public Nivel Nivel  { get; set; }
+        public List<Album> Albumes { get; set; }
+        public List<Figurita> Figuritas { get; set; }
+    }
+}
