7edc3b5140d8c67d44d87b4f14407a04429b34b7
Luciano <lucianoapugliese@gmail.com>
2023-06-19 17:49:41 -0300

    Añadidas clases 'Estadio' y 'EstadioNegocio'

---
+++ TPCuatrimestral-Album/Test/EstadioNegocio.cs
@@ -0,0 +1,138 @@
+﻿using dominio2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class EstadioNegocio
+    {
+        public List<Estadio> listar()
+        {
+            List<Estadio> estadios = new List<Estadio>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT ID, Nombre, Alias, Capacidad, Barrio, Calle, Imagen FROM Estadios");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Estadio estadio = new Estadio();
+                    estadio.ID = (int)datos.Lector["ID"];
+                    estadio.Nombre = (string)datos.Lector["Nombre"];
+                    estadio.Alias = datos.Lector["Alias"] is DBNull ? "-" : (string)datos.Lector["Alias"];
+                    estadio.Capacidad = (int)datos.Lector["Capacidad"];
+                    estadio.Barrio = (string)datos.Lector["Barrio"];
+                    estadio.Calle = (string)datos.Lector["Calle"];
+                    estadio.Imagen = (string)datos.Lector["Imagen"];
+                    estadios.Add(estadio);
+                }
+                return estadios;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public Estadio listar(int ID)
+        {
+            Estadio estadio = new Estadio();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta($"SELECT ID, Nombre, Alias, Capacidad, Barrio, Calle, Imagen FROM Estadios WHERE ID ='{ID}'");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    estadio.ID = (int)datos.Lector["ID"];
+                    estadio.Nombre = (string)datos.Lector["Nombre"];
+                    estadio.Alias = datos.Lector["Alias"] is DBNull ? "-" : (string)datos.Lector["Alias"];
+                    estadio.Capacidad = (int)datos.Lector["Capacidad"];
+                    estadio.Barrio = (string)datos.Lector["Barrio"];
+                    estadio.Calle = (string)datos.Lector["Calle"];
+                    estadio.Imagen = (string)datos.Lector["Imagen"];
+                }
+                return estadio;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public Estadio modificar(Estadio estadio)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta($"UPDATE ESTADIOS SET Nombre = '{estadio.Nombre}', Alias = '{estadio.Alias}', Capacidad = '{estadio.Capacidad}', Barrio = '{estadio.Barrio}', Calle = '{estadio.Calle}', Imagen = '{estadio.Imagen}' FROM Estadios WHERE ID = '{estadio.ID}'");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    estadio.ID = (int)datos.Lector["ID"];
+                    estadio.Nombre = (string)datos.Lector["Nombre"];
+                    estadio.Alias = datos.Lector["Alias"] is DBNull ? "-" : (string)datos.Lector["Alias"];
+                    estadio.Capacidad = (int)datos.Lector["Capacidad"];
+                    estadio.Barrio = (string)datos.Lector["Barrio"];
+                    estadio.Calle = (string)datos.Lector["Calle"];
+                    estadio.Imagen = (string)datos.Lector["Imagen"];
+                }
+                return estadio;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public void agregar(Estadio estadio)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta($"INSERT INTO ESTADIOS (Nombre, Alias, Capacidad, Barrio, Calle, Imagen) VALUES('{estadio.Nombre}','{estadio.Alias}','{estadio.Capacidad}','{estadio.Barrio}','{estadio.Calle}','{estadio.Imagen}')");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public void eliminar(int ID)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta($"DELETE FROM ESTADIOS WHERE ID ='{ID}'");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ TPCuatrimestral-Album/dominio2/Estadio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio2
+{
+    public class Estadio
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Alias { get; set; }
+        public int Capacidad { get; set; }
+        public string Barrio { get; set; }
+        public string Calle { get; set; }
+        public string Imagen { get; set; }
+        public bool Activo { get; set; }
+    }
+}
+//ID SMALLINT PRIMARY KEY IDENTITY(1,1),
+//Nombre VARCHAR(100) NOT NULL,
+//Alias VARCHAR(50),
+//Capacidad VARCHAR(5) NOT NULL CHECK (Capacidad LIKE '%[0-9]%'),
+//Barrio VARCHAR(50),
+//Calle VARCHAR(150),
+//Imagen VARCHAR(250),
+//Activo BIT DEFAULT 1
